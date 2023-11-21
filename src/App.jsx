3b5bc664545d7148faac690dce242d78b5e459ee
josemfiguero04 <josemfigueroa04@gmail.com>
2023-11-21 18:02:57 -0300

    creacion de page

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Home from './pages/Home/Home.jsx';
+import MyAccount from './pages/MyAccount/MyAccount.jsx';
+import MyOrder from './pages/MyOrder/MyOrder.jsx';
+import MyOrders from './pages/MyOrders/MyOrders.jsx';
+import NotFound from './pages/NotFound/NotFound.jsx';
+import SignIn from './pages/SignIn/SignIn';
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
-    <>
-      <div>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+  
+      <div className='bg-red-300'>
+        <Home />
+        <MyAccount />
+        <MyOrder />
+        <MyOrders />
+        <NotFound />
+        <SignIn />
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+  
   )
 }
 
-export default App
+export default App;
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,9 @@
+function Home () {
+  return (
+    <div className='bg-red-300'>
+      <h1>Home</h1>
+    </div>
+  )
+}
+
+export default Home;
