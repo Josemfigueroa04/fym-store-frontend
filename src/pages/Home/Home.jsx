cc6fe89ab43733ea22406ccd00e1e80fdc70224a
josemfiguero04 <josemfigueroa04@gmail.com>
2023-11-22 17:22:09 -0300

    feature card

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,14 @@
-import Layout from "../../components/Layout/Layout";
+import Card from "../../components/Card/Card.jsx";
 
 function Home () {
   return (
     
+    <div>
 
         <h1>Home</h1>
+        <Card />
+    </div>
+
     
     
   )
